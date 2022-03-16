5c304bce219ca23a9473798e8ca47f5d972d90ec
MARCOS SAMUEL BORGES DAMASCENO <marcos-samuel2@hotmail.com>
2022-03-16 15:04:53 -0300

    Models, controllers and reposirotys created

---
+++ src/main/java/com/eventoapp/EventoappApplication.java
@@ -0,0 +1,17 @@
+package com.eventoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
+public class EventoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoappApplication.class, args);
+	}
+
+}
